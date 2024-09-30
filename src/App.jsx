c57a24cdab8be6c6646ae60872b49b40dffbe9e6
mamta1998gola <mamta1998gola@gmail.com>
2024-09-30 09:56:33 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -29,34 +29,29 @@ function Root() {
     }
   }
 
-  const getUserData = async () => {
-    try {
-      const response = await fetch(`${API}/userdata`, {
+  const getUserData = () => {
+    if (sessionStorage.getItem('token')) {
+      fetch(`${API}/userdata`, {
         method: "POST",
         headers: {
           'Content-Type': 'application/json'
         },
         credentials: 'include',
         body: JSON.stringify({ token: JSON.parse(sessionStorage.getItem('token')) ?? '' })
-      });
+      })
+        .then(res => res.json())
+        .then(response => {
+          setUser(prev => ({
+            ...prev,
+            email: response.data.email
+          }));
+        })
+        .catch(err => { throw new Error(err) })
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const userData = await response.json();
-      setUser(prev => ({
-        ...prev,
-        email: userData.data.email
-      }));
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-      // Handle the error appropriately (e.g., show an error message to the user)
     }
   }
 
   useEffect(() => {
-
     fetch('https://to-do-server-app.vercel.app/test-cors', {
       method: 'GET',
       credentials: 'include',
@@ -67,7 +62,7 @@ function Root() {
 
     if (user.email !== '') {
       fetchTodos();
-    } else {
+    } else if (sessionStorage.getItem('token')) {
       getUserData();
     }
   }, [user.email]);
